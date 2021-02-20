7ed9fdb42e0b069390b2c695e2f127d9c946d331
Littig <littig.develop@gmail.com>
2021-02-20 12:18:09 -0300

    User creation and refactoring routines in the appointments route, changing provider to provider_id

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -16,13 +16,13 @@ appointmentRouter.get('/', async (resquest, response) => {
 
 appointmentRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({ date: parsedDate, provider });
+    const appointment = await createAppointment.execute({ date: parsedDate, provider_id });
 
     return response.status(200).json(appointment);
   } catch (err) {
